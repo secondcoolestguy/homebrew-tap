67144dde01f7a5679178e9addcd9c2fc88d8a2a2
secondcoolestguy <secondcoolestguy@gmail.com>
2026-07-17 18:00:02 +0300

    Update ahola.rb

--- ahola.rb
+++ ahola.rb
@@ -1,7 +1,7 @@
 class Ahola < Formula
   desc "The ultra-fast, dual-dialect systems programming language engine"
-  homepage "https://github.com/yourusername/ahola"
-  url "https://github.com/yourusername/ahola/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/secondcoolestguy/ahola"
+  url "https://github.com/secondcoolestguy/ahola/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "e7f135b129b16217ea72b01f779baf9a004f78b17449cd44c88243fc63be210e"
   license "GNU v3.0"
 
